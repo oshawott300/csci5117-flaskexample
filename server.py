9e905d87d855c70680d641d9fd68663137a0960b
John <lei00007@umn.edu>
2023-02-06 11:38:25 -0600

    added gunicorn

--- server.py
+++ server.py
@@ -11,7 +11,18 @@ def hi():
   user_name = request.args.get("userName", "unknown")
   return render_template('main.html', user=user_name) 
 
-@app.route('/fanClub', methods=['GET'])
+@app.route('/fanClub', methods=['GET', 'POST'])
 def atriFan():
 #   user_name = request.args.get("userName", "unknown")
+  if request.method == 'POST':
+    print(request.get('postTitle'))
+    return render_template('index.html') 
   return render_template('index.html') 
+
+# @app.route('/submit', methods=['GET','POST'])
+# def atriFan():
+#   if request.method = 'POST':
+#     user_name = request.get('postTitle')
+#   else:
+#     user_name = " "
+#   return render_template('check.html', user_name = user_name) 
